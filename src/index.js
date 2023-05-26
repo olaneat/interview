a02eb6e0397b0552abc9c36b2999d52f9a9ba696
tosin ayoola <tosinayoola0@gmail.com>
2023-05-26 17:49:53 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Index from './components/index/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>
 );
 
